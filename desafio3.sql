278dbae1add6c9af62ce0f2f715bcc8b21b87ef8
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-14 17:24:49 -0300

    Resolvido desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT user.`name` AS usuario,
+COUNT(ouvidas.user_fk) AS qtde_musicas_ouvidas,
+ROUND(SUM(musicas.duration) / 60, 2) AS total_minuto
+FROM SpotifyClone.users AS user
+INNER JOIN  SpotifyClone.history_reproduction AS ouvidas
+ON user.user_id = ouvidas.user_fk
+INNER JOIN SpotifyClone.sounds AS musicas
+ON ouvidas.sound_fk = musicas.sound_id
+GROUP BY user.name
+ORDER BY usuario ASC;
