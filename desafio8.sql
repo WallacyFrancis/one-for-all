0af42aa78f281b6f212dac76a10aa06deddbaa42
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-18 19:47:16 -0300

    Refatorando requisito 9

--- desafio8.sql
+++ desafio8.sql
@@ -1,3 +1,4 @@
+SELECT cantor.name AS artista,
 colection.nameAS album
 FROM SpotifyClone.artists AS cantor
 INNER JOIN SpotifyClone.albuns AS colection
--- desafio9.sql
+++ desafio9.sql
@@ -2,4 +2,4 @@ SELECT COUNT(historico.sound_fk) AS quantidade_musicas_no_historico
 FROM SpotifyClone.history_reproduction AS historico
 JOIN SpotifyClone.users AS usuario
 WHERE historico.user_fk = usuario.user_id
-AND usuario.`name` = 'Bill';
+AND usuario.name = 'Bill';
