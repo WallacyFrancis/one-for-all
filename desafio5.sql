03cd476ad78ee412c084975e7c196b360a87f72e
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-18 18:48:42 -0300

    Refatorando requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,8 @@
-COUNT(reproduction.sound_fk) AS reproducoes
+SELECT musica.name AS cancao,
+COUNT(history.sound_fk) AS reproducoes
 FROM SpotifyClone.sounds AS musica
-INNER JOIN SpotifyClone.history_reproduction AS reproduction
-ON reproduction.sound_fk = musica.sound_id
-GROUP BY musica.`name`
-ORDER BY musica.`name` ASC, cancao
-LIMIT 2;
+INNER JOIN SpotifyClone.history_reproduction AS history
+ON history.sound_fk = musica.sound_id
+GROUP BY history.sound_fk
+ORDER BY reproducoes DESC,
+cancao LIMIT 2;
