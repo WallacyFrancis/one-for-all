f455feba271faf7d632f00b20d5f4d3691b8fb67
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-18 20:00:51 -0300

    Reaftorando requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,12 @@
-SELECT musica.`name` AS nome,
-COUNT(musica.`name`) AS reproducoes
-FROM SpotifyClone.`sounds` AS musica
+SELECT musica.name AS nome,
+COUNT(historico.sound_fk) AS reproducoes
+FROM SpotifyClone.sounds AS musica
 JOIN SpotifyClone.history_reproduction AS historico
 JOIN SpotifyClone.users AS usuario
 JOIN SpotifyClone.plans AS plano
 ON (plano.plan = 'gratuito' OR plano.plan = 'pessoal')
 AND usuario.user_id = historico.user_fk
 AND historico.sound_fk = musica.sound_id
-GROUP BY musica.`name`;
+AND plano.plan_id = usuario.plan_fk
+GROUP BY musica.name
+ORDER BY musica.name ASC;
