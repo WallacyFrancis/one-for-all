151372499cdcee9c73f60c31936c577039c750d5
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-18 19:33:47 -0300

    Refatorando requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,11 @@
-SELECT cantor.`name` AS artista,
-colection.`name` AS album,
+-- Auxiliado pór Ariane Ueti na última linha
+SELECT cantor.name AS artista,
+colection.name AS album,
 COUNT(seguindo.artist_fk) AS seguidores
 FROM SpotifyClone.artists AS cantor
 JOIN SpotifyClone.albuns AS colection
-JOIN SpotifyClone.`follows` AS seguindo
+JOIN SpotifyClone.follows AS seguindo
 WHERE colection.artist_fk = cantor.artist_id
 AND cantor.artist_id = seguindo.artist_fk
-GROUP BY artista, album;
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
