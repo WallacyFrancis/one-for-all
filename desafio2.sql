b95b66930b7c4cded33aa33e804b973dedd2f7ec
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-18 17:25:39 -0300

    Corrigindo requisito 1 - 4 + corrigindo requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,4 @@
-SELECT COUNT(sound_id) AS cancoes,
-COUNT(DISTINCT artist_id) AS artistas,
-COUNT(DISTINCT album_id) AS albuns
-FROM SpotifyClone.`sounds` AS music
-INNER JOIN SpotifyClone.artists AS ar
-INNER JOIN SpotifyClone.albuns AS al
-ON music.album_fk = ar.artist_id
-AND al.artist_fk = ar.artist_id;
+-- Auxiliado por Coruja
+SELECT (SELECT COUNT(sound_id) FROM SpotifyClone.sounds) AS cancoes,
+(SELECT COUNT(artist_id) FROM SpotifyClone.artists) AS artistas,
+COUNT(DISTINCT album_id) AS albuns FROM SpotifyClone.albuns;
