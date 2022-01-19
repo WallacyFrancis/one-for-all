547ab0560b8fef619d312a5d4e3055a27229e7ac
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-19 13:45:30 -0300

    Corrigindo requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT cantor.name AS artista,
-colection.nameAS album
+colection.name AS album
 FROM SpotifyClone.artists AS cantor
 INNER JOIN SpotifyClone.albuns AS colection
 WHERE cantor.name = 'Walter Phoenix'
