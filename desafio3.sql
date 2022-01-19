980a479d012d5c6356e4d65561dfcb7f54fcd191
Wallacy Francis <wallacy_francis@hotmail.com>
2022-01-19 14:13:48 -0300

    Corrigindo requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT user.name AS usuario,
 COUNT(ouvidas.user_fk) AS qtde_musicas_ouvidas,
-ROUND(SUM(musicas.duration) / 60, 2) AS total_minuto
+ROUND(SUM(musicas.duration) / 60, 2) AS total_minutos
 FROM SpotifyClone.users AS user
 INNER JOIN  SpotifyClone.history_reproduction AS ouvidas
 ON user.user_id = ouvidas.user_fk
